7f85b9c8bb7dae1c54899ff8790bb85e000d0e0f
Chase Southard <chase.southard@gmail.com>
2008-12-12 13:06:44 -0500

    removed create account link from login. need to make helper for this when account.nil?

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+
+def need_to_sign_up?
+
+    # need to have this just as a redundant measure 
+   #<p>Don't have an account? <%= link_to "Sign up!", signup_path %></p>
+end
+
 end
